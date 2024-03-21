78e9e05d2e3a5163b584c3f0042c87a5227d1842
jfarr <jonothan.farr@gmail.com>
2024-03-20 17:07:50 -0700

    refactor

--- scenes/mob.gd
+++ scenes/mob.gd
@@ -1,54 +1,87 @@
-class_name MOB extends Node2D
+class_name MOB extends CharacterBody2D
 
-@onready var character : CharacterBody2D = get_owner()
-
-enum State {
+enum {
 	IDLE,
 	NEW_DIR,
-	MOVE
+	MOVE,
 }
+
+class MOBState extends State:
+
+	var mob : MOB
+
+	func _init(_mob : MOB):
+		mob = _mob
+
+class IdleState extends MOBState:
+
+	func enter():
+		mob.sprite.play("idle")
+
+class MoveState extends MOBState:
+
+	func physics_update(delta : float):
+		var distance = ((mob.position + mob.direction * mob.walk_speed) - mob.starting_pos).length()
+		if distance < mob.max_distance:
+			mob.move(mob.walk_speed)
+			mob.play_animation()
+		else:
+			mob.transition_state(NEW_DIR)
+			mob.move_timer.wait_time = 0.5
+			mob.sprite.play("idle")
+
+class NewDirectionState extends MOBState:
+	
+	func enter():
+		mob.direction = mob.choose([Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN])
+		mob.sprite.play("idle")
+
+@onready var states : Array[MOBState] = [
+	IdleState.new(self),
+	NewDirectionState.new(self),
+	MoveState.new(self),
+]
+
+func transition_state(next_state : int):
+	state = states[next_state]
+	state.enter()
 
 @export var sprite : AnimatedSprite2D
 @export var walk_speed = 30
 @export var max_distance = 120
 
-var state = State.IDLE
+var state : MOBState
 var direction = Vector2.RIGHT
 var starting_pos : Vector2
+var move_timer = Timer.new()
 
 func _ready():
 	randomize()
-	starting_pos = character.position
+	starting_pos = position
+	setup_timer()
+	transition_state(IDLE)
+
+func setup_timer():
+	move_timer.timeout.connect(_on_timer_timeout)
+	move_timer.set_wait_time(0.5)
+	move_timer.set_autostart(true)
+	add_child(move_timer)
 
 func _physics_process(delta : float):
-	match state:
-		State.IDLE:
-			sprite.play("idle")
-		State.NEW_DIR:
-			direction = choose([Vector2.RIGHT, Vector2.UP, Vector2.LEFT, Vector2.DOWN])
-			sprite.play("idle")
-		State.MOVE:
-			move(walk_speed)
-			play_animation()
-			var distance = ((character.position + direction * walk_speed) - starting_pos).length()
-			print("distance: %s" % distance)
-			if distance < max_distance:
-				move(walk_speed)
-			else:
-				state = State.NEW_DIR
-				$Timer.wait_time = 0.5
+	state.physics_update(delta)
 
 func choose(choices):
 	choices.shuffle()
 	return choices.front()
 
 func move(speed):
-	character.velocity = direction * speed
-	character.move_and_slide()
+	velocity = direction * speed
+	move_and_slide()
 
 func _on_timer_timeout():
-	$Timer.wait_time = choose([0.5, 1.0, 1.5])
-	state = choose([State.IDLE, State.NEW_DIR, State.MOVE])
+	move_timer.wait_time = choose([0.5, 1.0, 1.5])
+	var new_state = choose([IDLE, NEW_DIR, MOVE])
+	transition_state(new_state)
 
 func play_animation():
 	if direction.x == -1:
