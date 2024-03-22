76acf4369dcc66f2647d8a88cab705eea2d34cb2
jfarr <jonothan.farr@gmail.com>
2024-03-22 15:24:43 -0700

    refactor

--- scripts/item_spawn.gd
+++ scripts/item_spawn.gd
@@ -1,10 +1,10 @@
-class_name Lootable extends Node2D
+class_name ItemSpawn extends Node2D
 
 @export var item_scene : PackedScene
 @export var wait_time : int = 0
 
 var timer : Timer = Timer.new()
-var spawned = null
+var spawned : Collectible = null
 
 func _ready():
 	timer.wait_time = 0.1
@@ -13,16 +13,13 @@ func _ready():
 	timer.timeout.connect(spawn_item)
 	add_child(timer)
 
-func _process(delta):
-	pass
-
 func spawn_item():
 	spawned = item_scene.instantiate()
-	spawned.item.item_collected.connect(on_item_collected)
-	spawned.global_position = global_position
 	get_parent().add_child(spawned)
+	spawned.global_position = global_position
+	spawned.item.item_collected.connect(on_item_collected)
 
 func on_item_collected(object):
-	if wait_time > 0 and object == spawned:
+	if object == spawned:
 		timer.wait_time = wait_time
 		timer.start()
